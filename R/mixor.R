2a9a67672767c6c2c38173711a600286e9d3e5ee
Kellie J. Archer <archer.43@osu.edu>
2018-06-14 20:17:56 +0000

    version 1.0.4

--- R/mixor.R
+++ R/mixor.R
@@ -1,7 +1,7 @@
 mixor <-
 function (formula, data, id, which.random.slope = NA, subset, 
     weights, exclude.fixed.effect = NA, CONV = 1e-04, empirical.prior = FALSE, 
-    quadrature.dist = "Normal", NQ1 = 11, adaptive.quadrature = TRUE, 
+    quadrature.dist = "Normal", nAGQ = 11, adaptive.quadrature = TRUE, 
     link = "probit", KG = 0, KS = 0, IADD = -1, indep.re = FALSE, 
     random.effect.mean = TRUE, UNID = 0, vcov = TRUE) 
 {
@@ -46,64 +46,81 @@ function (formula, data, id, which.random.slope = NA, subset,
     }
     CHOL <- ifelse(vcov, 0, 1)
     if (is.empty.model(mt)) {
-        print("No predictor variables specified")
-        break
+        stop("No predictor variables specified")
     }
     else {
         W <- model.matrix(mt, mf, contrasts)
     }
-    is.int <- !identical(grep("(Intercept)", dimnames(W)[[2]]), integer(0))
-    if (length(which.random.slope) == 1 && is.na(which.random.slope)) { 
-                X <- W[, "(Intercept)", drop=FALSE]
-                dimnames(X)[[2]] <- list("(Intercept)")
-				if (random.effect.mean) { 
-					W <- W[,-grep("(Intercept)",dimnames(W)[[2]]), drop=FALSE]
-				}
-        } else if (UNID == 0 & is.int) {
-                X <- W[, c(1, which.random.slope + 1), drop=FALSE]
-                if (random.effect.mean | (length(exclude.fixed.effect) == 1 & !is.na(exclude.fixed.effect)) | length(exclude.fixed.effect) > 1) {
-                        remove <- c(1, which.random.slope+1, exclude.fixed.effect+1)
-                        remove <- remove[!is.na(remove)]
-                        W <- W[, -remove, drop=FALSE]
-                } 
-        } else if (UNID == 0 & !is.int) {                  
-                X <- W[, which.random.slope, drop=FALSE]      
-                if (random.effect.mean | (length(exclude.fixed.effect) == 1 & !is.na(exclude.fixed.effect)) | length(exclude.fixed.effect) > 1) {                     # new
-                        remove <- c(which.random.slope, exclude.fixed.effect)
-                        remove <- remove[!is.na(remove)]
-                        W <- W[, -remove, drop=FALSE]
-                } 
-    } else if (UNID == 1 & !is.int) {
+    is.int <- !identical(grep("(Intercept)", dimnames(W)[[2]]), 
+        integer(0))
+    if (length(which.random.slope) == 1 && is.na(which.random.slope)) {
+        X <- W[, "(Intercept)", drop = FALSE]
+        dimnames(X)[[2]] <- list("(Intercept)")
+        if (random.effect.mean) {
+            W <- W[, -grep("(Intercept)", dimnames(W)[[2]]), 
+                drop = FALSE]
+        }
+    }
+    else if (UNID == 0 & is.int) {
+        X <- W[, c(1, which.random.slope + 1), drop = FALSE]
+        if (random.effect.mean | (length(exclude.fixed.effect) == 
+            1 & !is.na(exclude.fixed.effect)) | length(exclude.fixed.effect) > 
+            1) {
+            remove <- c(1, which.random.slope + 1, exclude.fixed.effect + 
+                1)
+            remove <- remove[!is.na(remove)]
+            W <- W[, -remove, drop = FALSE]
+        }
+    }
+    else if (UNID == 0 & !is.int) {
+        X <- W[, which.random.slope, drop = FALSE]
+        if (random.effect.mean | (length(exclude.fixed.effect) == 
+            1 & !is.na(exclude.fixed.effect)) | length(exclude.fixed.effect) > 
+            1) {
+            remove <- c(which.random.slope, exclude.fixed.effect)
+            remove <- remove[!is.na(remove)]
+            W <- W[, -remove, drop = FALSE]
+        }
+    }
+    else if (UNID == 1 & !is.int) {
         X <- W[, which.random.slope]
-        if ((length(exclude.fixed.effect) == 1 & !is.na(exclude.fixed.effect)) | length(exclude.fixed.effect) > 1) {
+        if ((length(exclude.fixed.effect) == 1 & !is.na(exclude.fixed.effect)) | 
+            length(exclude.fixed.effect) > 1) {
             W <- W[, -exclude.fixed.effect]
         }
-          if (random.effect.mean) {
-        W <- W[, -grep("(Intercept)", dimnames(W)[[2]]),drop=FALSE]
-          }
-    } else if (UNID == 1 & is.int) {
+        if (random.effect.mean) {
+            W <- W[, -grep("(Intercept)", dimnames(W)[[2]]), 
+                drop = FALSE]
+        }
+    }
+    else if (UNID == 1 & is.int) {
         X <- W[, which.random.slope + 1]
-        if ((length(exclude.fixed.effect) == 1 & !is.na(exclude.fixed.effect)) | length(exclude.fixed.effect) > 1) {
+        if ((length(exclude.fixed.effect) == 1 & !is.na(exclude.fixed.effect)) | 
+            length(exclude.fixed.effect) > 1) {
             W <- W[, -(exclude.fixed.effect + 1)]
         }
-          if (random.effect.mean) {
-        W <- W[, -grep("(Intercept)", dimnames(W)[[2]]),drop=FALSE]
-          }
+        if (random.effect.mean) {
+            W <- W[, -grep("(Intercept)", dimnames(W)[[2]]), 
+                drop = FALSE]
+        }
     }
-
     gam.labels <- character()
     if (ICEN == 1 & KG == 0) {
-        gam.labels <- paste("Threshold", ICODE[2:length(ICODE)], sep = "")
-    } else if (ICEN == 1 & KG > 0) {
+        gam.labels <- paste("Threshold", ICODE[2:length(ICODE)], 
+            sep = "")
+    }
+    else if (ICEN == 1 & KG > 0) {
         interactions <- expand.grid(ICODE[2:(length(ICODE))], 
             dimnames(W)[[2]][1:KG])
         gam.labels <- c(paste("Threshold", ICODE[2:(length(ICODE))], 
             sep = ""), paste("Threshold", interactions$Var1, 
             interactions$Var2, sep = ""))
-    }  else if (ICEN == 0 & KG == 0) {
+    }
+    else if (ICEN == 0 & KG == 0) {
         gam.labels <- paste("Threshold", ICODE[2:(length(ICODE) - 
             1)], sep = "")
-    } else if (ICEN == 0 & KG > 0) {
+    }
+    else if (ICEN == 0 & KG > 0) {
         interactions <- expand.grid(ICODE[2:(length(ICODE) - 
             1)], dimnames(W)[[2]][1:KG])
         gam.labels <- c(paste("Threshold", ICODE[2:(length(ICODE) - 
@@ -116,10 +133,13 @@ function (formula, data, id, which.random.slope = NA, subset,
     P <- ncol(W)
     if (UNID == 0 & IDIAG == 0) {
         RR <- R * (R + 1)/2
-    } else {
+    }
+    else {
         RR <- R
     }
-    if (ICEN == 1)  NGAM <- (MAXJ - 1) * (KG + 1) else NGAM <- (MAXJ - 2) * (KG + 1)
+    if (ICEN == 1) 
+        NGAM <- (MAXJ - 1) * (KG + 1)
+    else NGAM <- (MAXJ - 2) * (KG + 1)
     NPAR <- P + RR + NGAM + KS + (1 - NOMU) * R
     NPARR <- NPAR * (NPAR + 1)/2
     IRT <- R
@@ -140,7 +160,7 @@ function (formula, data, id, which.random.slope = NA, subset,
             1), AIC = as.double(0), SBC = as.double(0), DEV = as.double(0), 
         AICD = as.double(0), SBCD = as.double(0), as.double(CONV), 
         as.integer(MAXJ), as.integer(IWT), as.integer(IPRIOR), 
-        as.integer(IUNIF), as.integer(NQ1), as.integer(AQUAD), 
+        as.integer(IUNIF), as.integer(nAGQ), as.integer(AQUAD), 
         as.integer(NFN), as.integer(ICEN), as.integer(KG), as.integer(IADD), 
         as.integer(IDIAG), as.integer(NOMU), as.integer(UNID), 
         matrix(as.double(ICODE), MAXJ, 1), as.integer(CHOL), 
@@ -149,50 +169,60 @@ function (formula, data, id, which.random.slope = NA, subset,
         EBvar = matrix(as.double(0), N, IRT * (IRT + 1)/2), as.integer(IRT), 
         matrix(as.integer(CEN), NTOT, 1))
     dim(fit$IDER2) = c(NPAR, NPAR)
-    dimnames(fit$MU)[1]<-list(dimnames(X)[[2]])
-    dimnames(fit$ALPHA)[1]<-list(dimnames(W)[[2]])
-    if ( (length(which.random.slope) == 1 && is.na(which.random.slope)) | indep.re | UNID==1) {
-	dimnames(fit$SIGMA)[1]<-list(paste("Random",dimnames(X)[[2]],sep="."))
-    } else if (UNID==0) {
-      random.effects <- c(dimnames(X)[[2]])
-      re.elements <- expand.grid(random.effects, random.effects)
-      re.mat <- matrix(paste(re.elements$Var1, re.elements$Var2), 
-                ncol = length(random.effects))	
-	dimnames(fit$SIGMA)[1]<-list(re.mat[upper.tri(re.mat, diag = TRUE)])
+    dimnames(fit$MU)[1] <- list(dimnames(X)[[2]])
+    dimnames(fit$ALPHA)[1] <- list(dimnames(W)[[2]])
+    if ((length(which.random.slope) == 1 && is.na(which.random.slope)) | 
+        indep.re | UNID == 1) {
+        dimnames(fit$SIGMA)[1] <- list(paste("Random", dimnames(X)[[2]], 
+            sep = "."))
+    }
+    else if (UNID == 0) {
+        random.effects <- c(dimnames(X)[[2]])
+        re.elements <- expand.grid(random.effects, random.effects)
+        re.mat <- matrix(paste(re.elements$Var1, re.elements$Var2), 
+            ncol = length(random.effects))
+        dimnames(fit$SIGMA)[1] <- list(re.mat[upper.tri(re.mat, 
+            diag = TRUE)])
     }
     if (KS > 0) {
-    	dimnames(fit$TAU)[1]<-    list(paste("Scale",dimnames(W)[[2]],sep="."))
+        dimnames(fit$TAU)[1] <- list(paste("Scale", dimnames(W)[[2]], 
+            sep = "."))
     }
-	if (dim(fit$GAM)[1]!=0) {
-		dimnames(fit$GAM)[1]<-list(gam.labels)
-	}
+    if (dim(fit$GAM)[1] != 0) {
+        dimnames(fit$GAM)[1] <- list(gam.labels)
+    }
     if (random.effect.mean == TRUE && KS == 0) {
         Est <- rbind(fit$MU, fit$ALPHA, fit$SIGMA, fit$GAM)
-    } else if (random.effect.mean == TRUE && KS > 0) {
+    }
+    else if (random.effect.mean == TRUE && KS > 0) {
         Est <- rbind(fit$MU, fit$ALPHA, fit$TAU, fit$SIGMA, fit$GAM)
-    } else if (random.effect.mean==FALSE && KS==0){
+    }
+    else if (random.effect.mean == FALSE && KS == 0) {
         Est <- rbind(fit$ALPHA, fit$SIGMA, fit$GAM)
-    } else if (random.effect.mean==FALSE && KS > 0) {
+    }
+    else if (random.effect.mean == FALSE && KS > 0) {
         Est <- rbind(fit$ALPHA, fit$TAU, fit$SIGMA, fit$GAM)
-    } 
+    }
     Z <- Est/fit$SE
     P.value <- 2 * (1 - pnorm(abs(Z)))
     Model <- cbind(Estimate = Est, SE = fit$SE, Z = Z, P.value = P.value)
-    dimnames(Model)[[2]] <- c("Estimate", "Std. Error", "z value", "P(>|z|)")
+    dimnames(Model)[[2]] <- c("Estimate", "Std. Error", "z value", 
+        "P(>|z|)")
     varcov <- fit$IDER2
     dim(varcov) <- c(NPAR, NPAR)
     EBmean <- fit$EBmean
     EBvar <- fit$EBvar
     dimnames(EBmean)[[1]] <- unique(IDS)
     dimnames(EBvar)[[1]] <- unique(IDS)
-    object <- list(call = cl, Deviance = fit$DEV, Quadrature.points = NQ1, 
-        Model = Model, varcov = varcov, EBmean = EBmean, 
-        EBvar = EBvar, RIDGEMAX = fit$RIDGEMAX, RLOGL = fit$RLOGL, 
-        SE = fit$SE, AIC = fit$AIC, SBC = fit$SBC, AICD = fit$AICD, 
-        SBCD = fit$SBCD, MU=fit$MU, ALPHA=fit$ALPHA, SIGMA=fit$SIGMA, 
-        GAM=fit$GAM, TAU=fit$TAU, IADD=IADD, Y=Y, X=X, W=W, MAXJ=MAXJ,  
-        random.effect.mean=random.effect.mean, KS=KS, KG=KG, id=IDS, 
-        which.random.slope=which.random.slope, ICEN=ICEN, link=link, terms=mt)
+    object <- list(call = cl, Deviance = fit$DEV, Quadrature.points = nAGQ, 
+        Model = Model, varcov = varcov, EBmean = EBmean, EBvar = EBvar, 
+        RIDGEMAX = fit$RIDGEMAX, RLOGL = fit$RLOGL, SE = fit$SE, 
+        AIC = fit$AIC, SBC = fit$SBC, AICD = fit$AICD, SBCD = fit$SBCD, 
+        MU = fit$MU, ALPHA = fit$ALPHA, SIGMA = fit$SIGMA, GAM = fit$GAM, 
+        TAU = fit$TAU, IADD = IADD, Y = Y, X = X, W = W, MAXJ = MAXJ, 
+        random.effect.mean = random.effect.mean, KS = KS, KG = KG, 
+        id = IDS, which.random.slope = which.random.slope, ICEN = ICEN, 
+        link = link, terms = mt)
     class(object) <- "mixor"
     object
 }
--- inst/doc/mixor.R
+++ inst/doc/mixor.R
@@ -1,13 +1,13 @@
 ### R code from vignette source 'mixor.Rnw'
 
 ###################################################
-### code chunk number 1: mixor.Rnw:160-161
+### code chunk number 1: mixor.Rnw:159-160
 ###################################################
 options(prompt = "R> ", continue = "+  ", width = 70, useFancyQuotes = FALSE)
 
 
 ###################################################
-### code chunk number 2: mixor.Rnw:163-167
+### code chunk number 2: mixor.Rnw:162-166
 ###################################################
 library("mixor")
 data("schizophrenia")
@@ -16,13 +16,13 @@ SCHIZO1.fit <- mixor(imps79o ~ TxDrug + SqrtWeek + TxSWeek,
 
 
 ###################################################
-### code chunk number 3: mixor.Rnw:172-173
+### code chunk number 3: mixor.Rnw:171-172
 ###################################################
 summary(SCHIZO1.fit)
 
 
 ###################################################
-### code chunk number 4: mixor.Rnw:176-179
+### code chunk number 4: mixor.Rnw:175-178
 ###################################################
 print(SCHIZO1.fit)
 coef(SCHIZO1.fit)
@@ -30,7 +30,7 @@ vcov(SCHIZO1.fit)
 
 
 ###################################################
-### code chunk number 5: mixor.Rnw:183-187
+### code chunk number 5: mixor.Rnw:182-186
 ###################################################
 cm <- matrix(c(-1, 0, 0, 0, 0, 0, 0,
   -1, 0, 0, 0, 0, 1, 0, 
@@ -39,13 +39,13 @@ Contrasts(SCHIZO1.fit, contrast.matrix = cm)
 
 
 ###################################################
-### code chunk number 6: mixor.Rnw:192-193
+### code chunk number 6: mixor.Rnw:191-192
 ###################################################
 plot(SCHIZO1.fit)
 
 
 ###################################################
-### code chunk number 7: mixor.Rnw:199-203
+### code chunk number 7: mixor.Rnw:198-202
 ###################################################
 pihat <- predict(SCHIZO1.fit)
 names(pihat)
@@ -54,7 +54,7 @@ head(pihat$predicted)
 
 
 ###################################################
-### code chunk number 8: mixor.Rnw:208-211
+### code chunk number 8: mixor.Rnw:207-210
 ###################################################
 SCHIZO2.fit <- mixor(imps79o ~ TxDrug + SqrtWeek + TxSWeek, 
   data = schizophrenia, id = id, which.random.slope = 2, link = "logit")
@@ -62,13 +62,13 @@ summary(SCHIZO2.fit)
 
 
 ###################################################
-### code chunk number 9: mixor.Rnw:214-215
+### code chunk number 9: mixor.Rnw:213-214
 ###################################################
 names(SCHIZO2.fit)
 
 
 ###################################################
-### code chunk number 10: mixor.Rnw:218-222
+### code chunk number 10: mixor.Rnw:217-221
 ###################################################
 logLik(SCHIZO1.fit)
 deviance(SCHIZO1.fit)
@@ -77,7 +77,7 @@ BIC(SCHIZO1.fit)
 
 
 ###################################################
-### code chunk number 11: mixor.Rnw:226-230
+### code chunk number 11: mixor.Rnw:225-229
 ###################################################
 SCHIZO3.fit <- mixor(imps79o ~ TxDrug + SqrtWeek + TxSWeek, 
   data = schizophrenia, id = id, which.random.slope = 2, indep.re = TRUE, 
@@ -86,35 +86,35 @@ summary(SCHIZO3.fit)
 
 
 ###################################################
-### code chunk number 12: mixor.Rnw:255-261
+### code chunk number 12: mixor.Rnw:254-260
 ###################################################
 data("SmokeOnset")
 require("survival")
 Surv.mixord <- mixor( Surv(smkonset, event) ~ SexMale + cc + tv, 
-  data = SmokeOnset, id = class, link = "cloglog", NQ1 = 20, 
+  data = SmokeOnset, id = class, link = "cloglog", nAGQ = 20, 
   IADD = 1)
 summary(Surv.mixord)
 
 
 ###################################################
-### code chunk number 13: mixor.Rnw:264-267
+### code chunk number 13: mixor.Rnw:263-266
 ###################################################
 School.mixord <- mixor(Surv(smkonset, event) ~ SexMale + cc + tv, 
-  data = SmokeOnset, id = school, link = "cloglog", NQ1 = 20, IADD = 1)
+  data = SmokeOnset, id = school, link = "cloglog", nAGQ = 20, IADD = 1)
 summary(School.mixord)
 
 
 ###################################################
-### code chunk number 14: mixor.Rnw:272-276
+### code chunk number 14: mixor.Rnw:271-275
 ###################################################
 students.mixord <- mixor( Surv(smkonset, event) ~ SexMale + cc + tv, 
-  data = SmokeOnset, id = class, link = "cloglog", KG = 1, NQ1 = 20, 
+  data = SmokeOnset, id = class, link = "cloglog", KG = 1, nAGQ = 20, 
   IADD = 1)
 summary(students.mixord)
 
 
 ###################################################
-### code chunk number 15: mixor.Rnw:280-283
+### code chunk number 15: mixor.Rnw:279-282
 ###################################################
 LRtest <- deviance(Surv.mixord) - deviance(students.mixord)
 LRtest
@@ -122,7 +122,7 @@ pchisq(LRtest, 2, lower.tail=FALSE)
 
 
 ###################################################
-### code chunk number 16: mixor.Rnw:286-296
+### code chunk number 16: mixor.Rnw:285-295
 ###################################################
 cm <- matrix(c(1, 1, 0, 0,
   0, 0, 1, 1,
@@ -137,59 +137,59 @@ Contrasts(students.mixord, contrast.matrix = cm)
 
 
 ###################################################
-### code chunk number 17: mixor.Rnw:304-308
+### code chunk number 17: mixor.Rnw:303-307
 ###################################################
 data("norcag")
 Fitted.norcag <- mixor(SexItems ~ Item2vs1 + Item3vs1, 
-  data = norcag, id = ID, weights = freq, link = "logit", NQ1 = 20)
+  data = norcag, id = ID, weights = freq, link = "logit", nAGQ = 20)
 summary(Fitted.norcag)
 
 
 ###################################################
-### code chunk number 18: mixor.Rnw:313-317
+### code chunk number 18: mixor.Rnw:312-316
 ###################################################
 Fitted.norcag.np <- mixor(SexItems ~ Item2vs1 + Item3vs1, 
-  data = norcag, id = ID, weights = freq, link = "logit", NQ1 = 10, 
+  data = norcag, id = ID, weights = freq, link = "logit", nAGQ = 10, 
   KG = 2)
 summary(Fitted.norcag.np)
 
 
 ###################################################
-### code chunk number 19: mixor.Rnw:329-333
+### code chunk number 19: mixor.Rnw:328-332
 ###################################################
 Fitted.norcag.scale <- mixor(SexItems ~ Item2vs1 + Item3vs1, 
-  data = norcag, id = ID, weights = freq, link = "logit", NQ1 = 10, 
+  data = norcag, id = ID, weights = freq, link = "logit", nAGQ = 10, 
   KS = 2)
 summary(Fitted.norcag.scale)
 
 
 ###################################################
-### code chunk number 20: mixor.Rnw:344-346
+### code chunk number 20: mixor.Rnw:342-344
 ###################################################
 data("concen")
-concen<-concen[order(concen$ID),] 
+concen<-concen[order(concen$ID),]
 
 
 ###################################################
-### code chunk number 21: mixor.Rnw:349-352
+### code chunk number 21: mixor.Rnw:347-350
 ###################################################
 Common.ICC <- mixor(TConcen ~ Mz, data = concen, id = ID, 
-  weights = freq, link = "probit", NQ1 = 10)
+  weights = freq, link = "probit", nAGQ = 10)
 summary(Common.ICC)
 
 
 ###################################################
-### code chunk number 22: mixor.Rnw:356-361
+### code chunk number 22: mixor.Rnw:354-359
 ###################################################
 Varying.ICC <- mixor(TConcen ~ Mz + Dz, data = concen, id = ID, 
   weights = freq, which.random.slope = 1:2, exclude.fixed.effect = 2, 
-  link = "probit", NQ1 = 20, random.effect.mean = FALSE, 
+  link = "probit", nAGQ = 20, random.effect.mean = FALSE, 
   UNID = 1)
 summary(Varying.ICC)
 
 
 ###################################################
-### code chunk number 23: mixor.Rnw:364-367
+### code chunk number 23: mixor.Rnw:362-365
 ###################################################
 LRtest <- deviance(Common.ICC) - deviance(Varying.ICC)
 LRtest
